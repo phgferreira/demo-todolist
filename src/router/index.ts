92792e03e1a45d9576934877b208f56f05879b8c
Paulo Henrique Gonçalves Ferreira <phgferreira@outlook.com>
2023-04-30 19:44:01 -0300

    Adiciona tarefas em uma lista

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView/index.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/HomeView/index.vue')
   },
   // {
   //   path: '/about',
---
+++ src/views/HomeView/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Home Page</h1>
+    <v-container class="pa-16">
+        <v-text-field v-model="tarefa" placeholder="Digite a tarefa e tecle enter" @keyup.enter="adicionarTarefa" />
+        <v-list :lines="'one'">
+            <v-list-item v-for="tar in tarefas" :key="tar" :title="tar" />
+        </v-list>
+    </v-container>
+</template>
+
+<script lang="ts">
+import {ref} from "vue";
+
+export default {
+    name: 'HomeView',
+    setup() {
+        let tarefa = ref('');
+        let tarefas: string[] = [];
+
+        function adicionarTarefa() {
+            debugger;
+            tarefas.push(tarefa.value);
+            tarefa.value = '';
+            debugger;
+        }
+
+        return { tarefa, tarefas, adicionarTarefa}
+    }
+}
+</script>
