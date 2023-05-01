47e3d9ed2a8967cc8bb16fe3be0c66ac2fb32982
Paulo Henrique Gonçalves Ferreira <phgferreira@outlook.com>
2023-05-01 17:48:14 -0300

    Ajuste no placeholder do input de tarefas

--- src/views/HomeView/index.vue
+++ src/views/HomeView/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Home Page</h1>
     <v-container class="pa-16">
-        <v-text-field v-model="tarefa" placeholder="Digite a tarefa e tecle enter" @keyup.enter="adicionarTarefa" />
+        <v-text-field v-model="tarefa" placeholder="Digite a tarefa e tecle Enter" @keyup.enter="adicionarTarefa" />
         <v-list :lines="'one'">
             <v-list-item v-for="tar in tarefas" :key="tar" :title="tar" />
         </v-list>
