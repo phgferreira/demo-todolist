3865f8c9878ece8c9639c0c21b828af2703ed838
Paulo Ferreira <phgferreira@outlook.com>
2023-08-05 19:05:33 -0300

    Adicionado icone de trash-can na biblioteca para uso

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,15 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+// Fontawesome
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
 const app = createApp(App)
+
+library.add(faTrashCan)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 registerPlugins(app)
 
